4ac7d96c7123a5d77d3986907b925e26e1aea77c
austin4mlp <austinbaron.coding@gmail.com>
2018-01-10 07:05:30 -0500

    new api

--- app.js
+++ app.js
@@ -4,13 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var gAPIRoutes = require('./routes/globalAPI');
-var index = require('./routes/index');
-var expressValidator = require('express-validator');
-var session = require('./config/session').session;
 var cors = require('cors');
-var helmet = require('helmet');
-var csrf = require('csurf');
+var index = require('./routes/index');
+var api = require('./routes/api');
+var fs = require('fs');
+// test
+require('./config/db');
 
 var app = express();
 
@@ -22,22 +21,13 @@ app.set('view engine', 'hbs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(cors());
 app.use(logger('dev'));
-app.use(bodyParser.json({limit:'50mb'}));
-app.use(bodyParser.urlencoded({ extended: false, limit:'50mb' }));
-app.use(expressValidator());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session);
-app.use(helmet());
-app.use(express.static(path.join(__dirname, 'dist')));
-app.use(function(req, res, next){
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-app.use('/gAPI', gAPIRoutes);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/uploads')));
 app.use('/', index);
-
+app.use('/chat', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -46,29 +36,15 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
+// error handler
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
