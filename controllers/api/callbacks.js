e257fb5d7407df1af5d902a1320630089b09cc65
Austin Baron <austinbaron.coding@gmail.com>
2018-01-10 09:11:50 -0500

    new api

--- controllers/api/callbacks.js
+++
@@ -1,5 +0,0 @@
-var exports = module.exports = {};
-exports.get = require('./http/get').main;
-exports.post = require('./http/post').main;
-exports.put = require('./http/put').main;
-exports.delete = require('./http/delete').main;
