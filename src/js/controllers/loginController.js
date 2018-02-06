bf2c4c0660ea7609e4c832f53cc6c33bc53558fc
HAMRIC <1QADJ@vcs2go.net>
2018-02-06 09:46:28 -0500

    changes to login

--- src/js/controllers/loginController.js
+++ src/js/controllers/loginController.js
@@ -1,8 +1,26 @@
 // JavaScript Document
-app.controller('loginCon', function(login, $scope, $state){
-$scope.loginMain = function(user){
-	login(user);
-	$state.go("interface");
+app.controller('loginCon', function(login, $scope, $state, formInputValidate){
+$scope.loginMain = function(){
+	var errors = formInputValidate($scope.user);
+	if(errors.num == 0){
+            login($scope.user).then(function(response){
+                $state.go('interface');
+            }, function(error){
+                console.log(error);
+            });
+        } else {
+            $scope.errors = errors;
+		}
 };
 	$scope.user = {};
+	$scope.errors = {};
+$scope.validate = function(input, field){
+        var obj = {};
+        obj[field] = input;
+        var errs = formInputValidate(obj);
+        if(errs.num > 0){
+            $scope.errors[field] = errs[field];
+
+}
 });
+
