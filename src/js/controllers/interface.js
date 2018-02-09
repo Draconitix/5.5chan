2db923e8d2cfe43d5e46f55263b092e60b86b9eb
HAMRIC <1QADJ@vcs2go.net>
2018-02-09 09:24:27 -0500

    interface.js changes

--- src/js/controllers/interface.js
+++ src/js/controllers/interface.js
@@ -1,6 +1,8 @@
 // JavaScript Document
 app.controller('interfaceState', function($scope, $state, logout, $cookies){
-	if($cookies.get('accessToken') == undefined){};
+	if($cookies.get('accessToken') == undefined){
+		$state.go('login');
+	};
 	$scope.posts = [];
 	$scope.logout = function(){
 		$cookies.remove('accessToken');
