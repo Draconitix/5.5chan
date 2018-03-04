6e10b22565105fc87030fc46b85d85a1f6056e58
Austin Baron <austinbaron.coding@gmail.com>
2018-03-04 18:27:17 -0500

    unit test for message directive

---
+++ wikiDist/5.5chan Gonzo Directive/script.js
@@ -0,0 +1,24 @@
+var app = angular.module('msgUnitTest', []);
+
+app.config(function($interpolateProvider) {
+  $interpolateProvider.startSymbol('{[{');
+  $interpolateProvider.endSymbol('}]}');
+});
+
+app.controller('mainCon', function($scope, $sce){
+    $scope.messages = [{ parts: [{ type: 'text', text: 'asdf' }, { type: 'video', url: 'https://www.youtube.com/embed/cixW6rogZ48' }] }];
+    $scope.trustUrl = function(url){
+        return $sce.trustAsResourceUrl(url);
+    };
+});
+
+app.directive('chatMessage', function(){
+    return {
+        restrict: 'E',
+        scope: {
+            messages: '=messages',
+            authorize: '&'
+        },
+        templateUrl: 'chat-message.html'
+    }
+});
