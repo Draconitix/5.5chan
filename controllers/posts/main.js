2a5689e1ad98edd51a6b672e868b7ac6e2274bd9
austin4mlp <austinbaron.coding@gmail.com>
2018-04-26 18:16:36 -0400

    fixed chat does not exist bug

--- controllers/posts/main.js
+++ controllers/posts/main.js
@@ -16,45 +16,50 @@ io.on('connection', function(socket){
     var chatrooms;
     var joinedChat;
     socket.on('chatJoin', function(data){
-		chatListMain({}, 'GET', cb);
-        if(data.chatroom != undefined && data.user != undefined){
-            chatrooms = JSON.parse(chatList);
-            var chatExistsFunc = function(){
-                var exists = false;
-                var index = 0;
-                for(var i = 0; i < chatrooms.length; i++){
-                    if(chatrooms[i].name === data.chatroom){
-                        exists = true;
-                        index = i;
-                    }
+        var cJ = function(stat, response){
+            if(stat == 200){
+                chatrooms = response;
+                     if(data.chatroom != undefined && data.user != undefined){
+                    chatrooms = response;
+                    var chatExistsFunc = function(){
+                        var exists = false;
+                        var index = 0;
+                        for(var i = 0; i < chatrooms.length; i++){
+                            if(chatrooms[i].name === data.chatroom){
+                                exists = true;
+                                index = i;
+                            }
+                        }
+                        return { exists: exists, index: index };
+                    };
+                    var check = chatExistsFunc();
+                    var i = check.index;
+                    if(check.exists == true){
+                       if(chatrooms[i].private == false){
+                          socket.join(data.chatroom);
+                          joinedChat = data.chatroom;
+                          socket.emit('chatPromise', { name: data.chatroom });   
+                          console.log('Joined Chat')
+                       } else {
+                          var pCheck = chatrooms[i].users.indexOf(data.user);       
+                          if(pCheck != -1){
+                              socket.join(data.chatroom);
+                              console.log('Joined Chat')
+                              joinedChat = data.chatroom; 
+                              socket.emit('chatPromise', { name: data.chatroom });
+                          } else {
+                              socket.emit('chatPromise', { error: 'You are not in this private chat.' });
+                          }   
+                       }
+                   } else {
+                        socket.emit('chatPromise', { error: 'Chatroom does not exist.' });   
+                   }   
+                } else {
+                    socket.emit('chatPromise', { error: 'No user or chatroom given.' }); 
                 }
-                return { exists: exists, index: index };
-            };
-            var check = chatExistsFunc();
-            var i = check.index;
-            if(check.exists == true){
-               if(chatrooms[i].private == false){
-                  socket.join(data.chatroom);
-                  joinedChat = data.chatroom;
-                  socket.emit('chatPromise', { name: data.chatroom });   
-                  console.log('Joined Chat')
-               } else {
-                  var pCheck = chatrooms[i].users.indexOf(data.user);       
-                  if(pCheck != -1){
-                      socket.join(data.chatroom);
-                      console.log('Joined Chat')
-                      joinedChat = data.chatroom; 
-                      socket.emit('chatPromise', { name: data.chatroom });
-                  } else {
-                      socket.emit('chatPromise', { error: 'You are not in this private chat.' });
-                  }   
-               }
-           } else {
-                socket.emit('chatPromise', { error: 'Chatroom does not exist.' });   
-           }   
-        } else {
-            socket.emit('chatPromise', { error: 'No user or chatroom given.' }); 
+            }    
         }
+        chatListMain({}, 'GET', cJ);
     });
     socket.on('chatLeave', function(data){
         socket.leave(data.chatroom);
